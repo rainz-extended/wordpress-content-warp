067f8603fcab488264cc41bf176a3a7276d353c5
rainz <reverrainz@gmail.com>
2021-10-01 14:33:33 +0700

    modify comment on code

--- content-warp.php
+++ content-warp.php
@@ -1,15 +1,15 @@
 <?php
     /* DEFINE CONTENT ID */
     $content_id = 236;
-    /*TRY TI GET CONTENT AND HANDLE ERROR PREVENT FATAL PHP ERROR */
+    /* TRY TI GET CONTENT AND HANDLE ERROR PREVENT FATAL PHP ERROR */
     try {
-        /*GET CONTENT BY POST ID */
+        /* GET CONTENT BY POST ID */
         $content_data = get_post($content_id, ARRAY_A);
         $content_post_data = $content_data['post_content'];
-        /*Export content to destination */
+        /* Export content to destination */
         echo $content_post_data;
     } catch ( Exception $e ) {
-        /*IF NO CONTENT THIS SECTION WITH BE ACTIVATE AUTOMATIC */
+        /* IF NO CONTENT THIS SECTION WITH BE ACTIVATE AUTOMATIC */
         echo "post's id is not valid";
     }
 ?>
